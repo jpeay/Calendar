f9eb05f0fb8b723302093a1173decfc8ed783b3d
jpeay <jpeay03@gmail.com>
2018-02-11 17:07:05 -0500

    add react component

--- source/app.js
+++ source/app.js
@@ -1,2 +1,24 @@
-const who = 'world';
-console.log(`Hello ${who}`);
+//const who = 'world';
+//console.log(`Hello ${who}`);
+//
+
+import React from 'react'
+import { render } from 'react-dom'
+
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      who: "React"
+    }
+  }
+  render () {
+    return (
+      <div>
+        <p>Hello world!</p>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'))
